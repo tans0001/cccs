fde786d681707e53dac288e57b44c20217890dbd
tans0001 <149453059+tans0001@users.noreply.github.com>
2023-12-08 14:38:08 +0800

    1

--- JS/zhishiku.js
+++ JS/zhishiku.js
@@ -19,8 +19,3 @@ Array.from(divs).forEach(div => div.style.display = 'none');
 const selectedDiv = document.getElementById(divId);
 selectedDiv.style.display = 'block';
 }
-function showpriv(imageUrl) {
-	var prive=document.getElementById('prive');
-	prive.src=imageUrl;
-	prive.style.display='block';
-}
